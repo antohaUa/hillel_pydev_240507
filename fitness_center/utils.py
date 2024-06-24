f9dc68062760dd844e24d7b4e9c10865b2a67c10
antohaUa <antoha.ua@gmail.com>
2024-06-25 01:17:44 +0300

    Fixes

--- fitness_center/utils.py
+++ fitness_center/utils.py
@@ -8,15 +8,15 @@ from celery import Celery
 app = Celery('tasks', broker='pyamqp://guest@localhost')
 
 GMAIL_SMTP_PORT = 587
-GMAIL_SMTP = 'smtp@gmail.com'
+GMAIL_SMTP = 'smtp.gmail.com'
 
 
-@app.task()
+@app.task
 def send_email(receiver_email, message):
     """Sent email via gmail smtp."""
     sender_email = os.environ.get('smtp_sender')
     smtp_password = os.environ.get('smtp_password')
-    with smtplib.SMTP_SSL(GMAIL_SMTP, GMAIL_SMTP_PORT) as smtp_client:
+    with smtplib.SMTP(GMAIL_SMTP, GMAIL_SMTP_PORT) as smtp_client:
         smtp_client.ehlo()
         smtp_client.starttls(context=ssl.SSLContext(ssl.PROTOCOL_TLS))
         smtp_client.ehlo()
